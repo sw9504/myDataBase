ade07cd243f26a77ce6343a202fb04bcb8cf7424
sw9504 <sladewilson9504@gmail.com>
2022-09-27 16:51:36 -0300

    Initial commit with navgraph

---
+++ app/src/main/java/com/utn/mydatabase/fragments/ListFragment.kt
@@ -0,0 +1,21 @@
+package com.utn.mydatabase.fragments
+
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.utn.mydatabase.R
+
+class ListFragment : Fragment() {
+    lateinit var v : View
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        v = inflater.inflate(R.layout.fragment_list, container, false)
+        return v
+    }
+}
